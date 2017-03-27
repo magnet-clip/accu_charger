b13ce9e140d9dd048df6d5cf5b111fc8ce2ced7c
asd <sd>
2017-03-27 07:21:43 +0300

    Implementing logic

--- charger.ino
+++ charger.ino
@@ -5,13 +5,37 @@ void setup() {
   
    OCR2A = 255;  // top/overflow value is 159 => produces a 100 kHz PWM; 239 => 66,67 kHz
    OCR2B = 80; 
+
    pinMode(3, OUTPUT);  // enable the PWM output (you now have a PWM signal on digital pin 3)
 
-   pinMode(A0, INPUT);
+   pinMode(A0, INPUT); // current limit
+   pinMode(A1, INPUT); // current
+   pinMode(A7, INPUT); // voltage
+
+   pinMode(12, OUTPUT); // red led, not connected / damaged (steady) or charging with current (blinking)
+   pinMode(11, OUTPUT); // yellow led, charging with voltage
+   pinMode(10, OUTPUT); // green led, charging finished
 }
 
 void loop() {
-  int dutyCycle = analogRead(A0) >> 2;
-  OCR2B = dutyCycle;
+  //int dutyCycle = analogRead(A0) >> 2;
+  //OCR2B = dutyCycle;
+
+  int voltage = analogRead(A7);
+  int current = analogRead(A1);
+
+  // todo short curcuit protection!
+
+  if (voltage <= MIN_VOLTAGE) {
+    stopCharging();
+    reportError();
+  } else if (voltage <= CURRENT_VOLTAGE_THRESHOLD) {
+    chargeWithCurrent();
+  } else if (voltage <= MAX_VOLTAGE) {
+    chargeWithVoltage();
+  } else if (voltage >= MAX_VOLTAGE) {
+    stopCharging();
+    reportSuccess();
+  } 
 }
 
