d74b09a503b5b53375d14f7bb1bea7ff1cd70ab0
Rustam Guseynov <rustam.guseynov@thomsonreuters.com>
2017-03-27 17:21:35 +0300

    Update charger.ino

--- charger.ino
+++ charger.ino
@@ -1,3 +1,19 @@
+#define CURRENT_LIMIT_PIN A0
+#define CURRENT_SENSE_PIN A1
+#define VOLTAGE_SENSE_PIN A7
+
+#define PWM_PIN 3
+
+#define RED_LED_PIN 12
+#define YELLOW_LED_PIN 11
+#define GREEN_LED_PIN 10
+
+#define MIN_VOLTAGE (1024.0*10.0/19.0)
+#define CURRENT_VOLTAGE_THRESHOLD (1024.0*13.0/19.0)
+#define MAX_VOLTAGE (1024.0*13.5/19.0)
+
+
+
 void setup() {
    TCCR2A |= (1 << COM2B1); // Clear OC2B on Compare Match, set OC2B at BOTTOM,(non-inverting mode). for fast pwm
    TCCR2A |= (1 << WGM21) | (1 << WGM20); // Fast PWM mode; top OCRA; Update of OCRx at BOTTOM; TOV Flag Set on TOP
@@ -6,23 +22,20 @@ void setup() {
    OCR2A = 255;  // top/overflow value is 159 => produces a 100 kHz PWM; 239 => 66,67 kHz
    OCR2B = 80; 
 
-   pinMode(3, OUTPUT);  // enable the PWM output (you now have a PWM signal on digital pin 3)
+   pinMode(PWM_PIN, OUTPUT);  // enable the PWM output (you now have a PWM signal on digital pin 3)
 
-   pinMode(A0, INPUT); // current limit
-   pinMode(A1, INPUT); // current
-   pinMode(A7, INPUT); // voltage
+   pinMode(CURRENT_LIMIT_PIN, INPUT); // current limit
+   pinMode(CURRENT_SENSE_PIN, INPUT); // current
+   pinMode(VOLTAGE_SENSE_PIN, INPUT); // voltage
 
-   pinMode(12, OUTPUT); // red led, not connected / damaged (steady) or charging with current (blinking)
-   pinMode(11, OUTPUT); // yellow led, charging with voltage
-   pinMode(10, OUTPUT); // green led, charging finished
+   pinMode(RED_LED_PIN, OUTPUT); // red led, not connected / damaged (steady) or charging with current (blinking)
+   pinMode(YELLOW_LED_PIN, OUTPUT); // yellow led, charging with voltage
+   pinMode(GREEN_LED_PIN, OUTPUT); // green led, charging finished
 }
 
 void loop() {
-  //int dutyCycle = analogRead(A0) >> 2;
-  //OCR2B = dutyCycle;
-
-  int voltage = analogRead(A7);
-  int current = analogRead(A1);
+  int voltage = analogRead(VOLTAGE_SENSE_PIN);
+  int current = analogRead(CURRENT_SENSE_PIN);
 
   // todo short curcuit protection!
 
